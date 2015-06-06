cea6267d5c1476a0eecc3bc9c18cf3f62987a971
HankComer <rodent05@gmail.com>
2015-06-06 13:24:42 -0500

    asdf

--- SimilarToC/TokenLang.hs
+++ SimilarToC/TokenLang.hs
@@ -2,4 +2,20 @@ module TokenLang where
 
 
 
-data Token 
+data Token a
+ = Op String
+ | LParen
+ | RParen
+ | LBracket
+ | RBracket
+ | LBrace
+ | RBrace
+ | Z Int
+ | R Double
+ | S String
+ | N String
+ | EOF
+ | AST a deriving (Show, Eq)
+
+
+
