0078972c7e903f3200812030c3b4506db228e9ad
HankComer <rodent05@gmail.com>
2015-06-15 20:46:30 -0500

    implemented instances for Consumer a

--- BottomUp/TokenMonad.hs
+++ BottomUp/TokenMonad.hs
@@ -6,3 +6,38 @@ import Control.Monad (ap)
 
 
 newtype Consumer a b = Consumer {consume :: ([a] -> [(b, [a])])}
+
+item :: Consumer a a
+item = Consumer (\s -> case s of
+    [] -> []
+    (c:cs) -> [(c, cs)])
+
+instance Functor (Consumer a) where
+    fmap f (Consumer q) = Consumer (\s -> [(f r, s') | (r, s') <- q s])
+
+instance Monad (Consumer a) where
+    return x = Consumer (\s -> [(x, s)])
+    (Consumer p) >>= f = Consumer (\s -> concat [consume (f a) s' |
+                                        (a, s') <- p s])
+
+instance Applicative (Consumer a) where
+    pure = return
+    (<*>) = ap
+
+instance Alternative (Consumer a) where
+    empty = Consumer (\s -> [])
+    (Consumer p) <|> (Consumer q) = Consumer (\s -> p s ++ q s)
+    some p = do
+        a <- p
+        as <- many p
+        return (a:as)
+    many p = Consumer $ (\s -> case consume (some p <|> (return [])) s of
+        [] -> []
+        (x:xs) -> [x])
+
+
+sat :: (a -> Bool) -> Consumer a a
+sat p = do
+    c <- item
+    if p c then return c else empty
+
